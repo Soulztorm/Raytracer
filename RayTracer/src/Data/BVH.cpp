22b4026fabb0a353e879d2bbf10c4faef15d9627
Soulztorm <FRL@gmx.de>
2025-02-13 22:47:15 +0100

    kek

--- RayTracer/src/Data/BVH.cpp
+++ RayTracer/src/Data/BVH.cpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "BVH.h"
-#include "Intersections.h"
 
 BVH::BVH(const Scene& scene)
 {
@@ -187,4 +186,79 @@ SplitInfo BVH::ChooseSplitAxis(const BoundingBox& boundingBox, int triIndex, int
 	}
 
 	return result;
+}
+
+
+
+
+
+
+////////////////////////////////////////////////////
+// Fast ray/AABB intersection test.
+// Implementation inspired by zacharmarz.
+// https://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
+////////////////////////////////////////////////////
+float Intersections::intersectBB(const BoundingBox& bbox, Ray* ray)
+{
+	glm::vec3 l1 = (bbox.center - ray->Origin) * ray->DirectionInverse;
+	glm::vec3 l2 = bbox.extends * ray->DirectionInverse;
+
+	glm::vec3 tMin = l1 - l2;
+	glm::vec3 tMax = l1 + l2;
+
+	glm::vec3 t2 = glm::max(tMin, tMax);
+	float tFar = std::min(std::min(t2.x, t2.y), t2.z);
+
+	if (tFar < 0.0f)
+		return FLT_MAX;
+
+	glm::vec3 t1 = glm::min(tMin, tMax);
+	float tNear = std::max(std::max(t1.x, t1.y), t1.z);
+
+	if (tNear > tFar)
+		return FLT_MAX;
+
+	return tNear;
+}
+
+////////////////////////////////////////////////////
+// Fast, minimum storage ray/triangle intersection test.
+// Implementation inspired by Tomas Moller: http://www.graphics.cornell.edu/pubs/1997/MT97.pdf
+// Additional algorithm details: http://www.lighthouse3d.com/tutorials/maths/ray-triangle-intersection/
+////////////////////////////////////////////////////
+bool Intersections::intersectTri(const TriangleOptimized& tri, Ray* ray, BVHHitInfo& hitInfo)
+{
+	glm::vec3 h = glm::cross(ray->Direction, tri.e2);
+	float a = glm::dot(tri.e1, h);
+
+	if (a > -FLT_EPSILON && a < FLT_EPSILON) {
+		return false;
+	}
+
+	float f = 1.0f / a;
+	glm::vec3 s = ray->Origin - tri.v0;
+	float u = f * glm::dot(s, h);
+
+	if (u < 0.0f || u > 1.0f) {
+		return false;
+	}
+
+	glm::vec3 q = glm::cross(s, tri.e1);
+	float v = f * glm::dot(ray->Direction, q);
+
+	if (v < 0.0f || u + v > 1.0f) {
+		return false;
+	}
+
+	// at this stage we can compute t to find out where the intersection point is on the line
+	float t = f * glm::dot(tri.e2, q);
+
+	if (t > FLT_EPSILON) { // ray intersection
+		hitInfo.dist = t;
+		hitInfo.u = u;
+		hitInfo.v = v;
+		return true;
+	}
+
+	return false;
 }
--- RayTracer/src/Data/BVH.h
+++ RayTracer/src/Data/BVH.h
@@ -1,18 +1,50 @@
 #pragma once
 #include "Scene.h"
 #include "Ray.h"
-#include "BVHStructs.h"
 
-struct HitInfo {
-	glm::vec3 position = glm::vec3(0.f);
-	glm::vec3 normal = glm::vec3(0.f);
-	int materialIndex = -1;
+class BoundingBox
+{
+public:
+	void growToInclude(const TriangleOBJ& tri) {
+		glm::vec3 min = this->center - this->extends;
+		glm::vec3 max = this->center + this->extends;
+
+		for (int i = 0; i < 3; ++i) {
+			min = glm::min(min, tri.Vertices[i]);
+			max = glm::max(max, tri.Vertices[i]);
+		}
+
+		this->center = 0.5f * (min + max);
+		this->extends = 0.5f * (max - min);
+	}
+
+	float getArea() {
+		float xy = 2.0f * extends[0] * extends[1];
+		float yz = 2.0f * extends[1] * extends[2];
+		float zx = 2.0f * extends[2] * extends[0];
+		return xy + yz + zx;
+	}
+
+	glm::vec3 center = glm::vec3(0.0f);
+	glm::vec3 extends = glm::vec3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
 };
 
 struct SplitInfo {
 	char axis = 0;
 	float plane = 0.0f;
 	float cost = FLT_MAX;
+};
+
+struct Node {
+	BoundingBox boundingBox;
+	int index = -1;
+	int triangleCount = -1;
+};
+
+struct HitInfo {
+	glm::vec3 position = glm::vec3(0.f);
+	glm::vec3 normal = glm::vec3(0.f);
+	int materialIndex = -1;
 };
 
 class BVH {
@@ -30,6 +62,23 @@ protected:
 
 	std::vector<Node> m_nodes;
 	std::vector<TriangleOptimized> m_trianglesOptimized;
-
 };
 
+
+
+
+
+// Intersection stuff
+struct BVHHitInfo {
+	float dist = FLT_MAX - 1.0f;
+	float u = 0.0f;
+	float v = 0.0f;
+	int triIndex = -1;
+};
+
+class Intersections
+{
+public:
+	static float intersectBB(const BoundingBox& bbox, Ray* ray);
+	static bool intersectTri(const TriangleOptimized& tri, Ray* ray, BVHHitInfo& hitInfo);
+};
